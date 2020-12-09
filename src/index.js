c2a3b8bc517c909644511fdba2a3da611a51a118
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-12-09 14:01:17 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Welcome to Dominos!");
+  } else if (req.url === "/contact") {
+    const obj = {
+      phone: "18602100000",
+      email: "guestcaredominos@jublfood.com"
+    };
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(obj));
+  } else {
+    res.writeHead(404).end("page not found");
+  }
 }
+
+httpServer.listen(8081, () => {
+  console.log("Listening");
+});
 
 module.exports = httpServer;
